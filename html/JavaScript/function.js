83d15b6fb516c177bab9c4a074d59f3fb2f0db44
Devsk001 <srik3713@gmail.com>
2023-07-03 13:03:48 +0530

    Funciton Basic added

---
+++ html/JavaScript/function.js
@@ -0,0 +1,18 @@
+console.log("Function In Js")
+
+// basic Function Creation
+
+function basic(){
+    console.log("Welcome To Js World !");
+}
+
+basic();
+
+// html get input and show function
+
+function showdetails(name,age){
+
+     return alert(name + age + "Years old");
+}
+
+showdetails('webma' , 19);
