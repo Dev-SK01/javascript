a543820e720fcdbdb42f3a49a9814b874544c8b8
Devsk001 <srik3713@gmail.com>
2023-07-14 12:46:02 +0530

    Object finished

--- html/JavaScript/object.js
+++ html/JavaScript/object.js
@@ -1,14 +1,102 @@
 console.log('Reference DataType IN JS : ')
 
+// * objecr constructor syntax
 var student = new Object();
-student.name =' Devsk ';  
+student.name = ' Devsk ';
 student.dep = ' CSE ';
-student.age = 19 ;
-student.details = function (name,dep,age){
-    console.log(student.name);
-    console.log(student.dep);
-    console.log(student.age);
+student.age = 19;
+student.details = function (name, dep, age) {
+    console.log(this.name);
+    console.log(this.dep);
+    console.log(this.age);
 }
-// object funciton call 
-
+// ? object funciton call 
 student.details();
+
+// ? object literal syntax : 
+var employee = {
+    Name: 'XXXXX',
+    Age: 20,
+    ID: '001001',
+    profession: 'entreprenuer',
+    details: function () {
+        console.log('Name : ' + this.Name)
+        console.log('AGE  : ' + this.Age)
+        console.log('ID   : ' + this.ID)
+        console.log('Profession : ' + this.profession)
+        console.log('Hobby : ' + this.Hobby);
+        console.log('Status : ' + this.status);
+    }
+};
+
+// ? employee function call
+employee.details();
+
+// ? this keyword example
+var employee2 = employee;
+console.log(employee2);
+employee2.details();
+
+// ? [] Uses In Object : 
+
+employee['Hobby'] = 'Music';
+console.log(employee);
+
+// ? variable access by the use of []
+
+var hobby = 'Hobby';
+console.log(employee[hobby]);
+employee['status'] = null ;
+
+employee.details();
+
+// ? For In Loop Example
+
+for( prop in employee){
+    console.log(prop + ' : ' + employee[prop]);
+}
+
+// todo Understanding Object 2 
+
+console.log(" Understanding Objects Part  2 ")
+console.log("HasOwnProperty Method : ");
+function ownprop(obj){
+    console.log(obj.hasOwnProperty('Name'));
+    console.log(obj.hasOwnProperty('Hobby'));
+    console.log(obj.hasOwnProperty('Tostring'));
+
+};
+
+ownprop(employee);
+
+// ? Object Restriction of Properties
+
+Object.defineProperty(employee,'Name',{
+    configurable: true,
+    writable : false,
+    enumerable: false
+});
+
+employee.Name = " Srikanth";
+console.log(employee.Name);
+
+// ? Getter And Setter function
+
+Object.defineProperty(employee,'Age2',{
+    get :  function(){
+        return this.status;
+    },
+    set : function(age){
+        if(age >= 30){
+             this.status = "Married";
+        }
+        else{
+            this.status = 'Unmarried';
+        }
+    }
+
+});
+
+employee.Age2 = 20;
+//  ? after the updation 
+employee.details();
