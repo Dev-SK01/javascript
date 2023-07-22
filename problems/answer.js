114a04c413ca40bbf38925a4cbc3b9a22873b6b4
Devsk001 <srik3713@gmail.com>
2023-07-22 12:27:51 +0530

    Function & Scope Finished .

--- problems/answer.js
+++ problems/answer.js
@@ -1,35 +1,59 @@
 // Printing 1 to 10 ;
 
-let firstNum = 0 ;
-while(firstNum <= 10){
- console.log(`${firstNum}`);
- firstNum++ ;
+let firstNum = 0;
+while (firstNum <= 10) {
+   console.log(`${firstNum}`);
+   firstNum++;
 };
 
 // Printing Odd Number Using While Loop.
-let evenNum = 0 ;
-while(evenNum < 10){
-    ++evenNum; //1 -> 3 ....
-    console.log(evenNum);
-    evenNum ++ ; //2 --> 4
+let evenNum = 0;
+while (evenNum < 10) {
+   ++evenNum; //1 -> 3 ....
+   console.log(evenNum);
+   evenNum++; //2 --> 4
 };
 
 //  converting mail
 
-function removeMail(mail){
-   if(typeof mail === 'string'){
-    //check if is it mail or not.
-         if(mail.includes('@') === false){
+function removeMail(mail) {
+   var removed = [];
+   // looping the email array
+   for (i = 0; i < mail.length; i++) {
+
+      if (typeof mail[i] === 'string') {
+         //check if is it mail or not.
+         if (mail[i].includes('@') === false) {
             return console.log('Enter A correct Mail !')
          }
-         else{
-            var removed = mail.split('@');
-            return console.log('Email Sender Name : ' , removed[0]);
+         else {
+
+            removed.push(mail[i].slice(0, mail[i].indexOf('@')));
+
          }
+
+      }
+      else {
+         return console.log('Please Enter String Mail!');
+      }
       
    }
-   else{
-      return console.log('Please Enter String Mail!');
-   }
+   return console.table(removed);
 }
-removeMail('google@gmail.com');
+Email = ['google@gmail.com', 'Yahoo@mail.com', 'Facebook@mail.com', 'Instagram@mail.com'];
+removeMail(Email);
+
+// First case As Upper Case
+
+function asProperCase(mystring){
+   var upcase = mystring[0].toUpperCase();
+   var sli = mystring.slice(1).toLowerCase();
+   return console.log(`Input String : ${mystring}\n`,`Output String : ${upcase + sli}`);
+}
+
+asProperCase('welcome to JAVAscript');
+// let arr = str.split(' ');
+// console.log(arr);
+// console.log(str[0].toLocaleUpperCase())
+
+
